f65583ed3c3fdba42922a676c15acab843d279f5
Corey <cthur315@gmail.com>
2014-09-08 18:30:15 -0700

    More character loading

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -17,7 +17,7 @@ public class GameMaster : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_pc = Instantiate (playerCharacter, Vector3.zero, Quaternion.identity) as GameObject;
-		_pc.name = "pc";
+		_pc.name = "PC";
 		_pcScript = _pc.GetComponent<PlayerCharacter>();
 
 		zOffset = -2.5f;
@@ -26,14 +26,16 @@ public class GameMaster : MonoBehaviour {
 
 		mainCamera.transform.position = new Vector3( _pc.transform.position.x, _pc.transform.position.y + yOffset, _pc.transform.position.z + zOffset);
 		mainCamera.transform.Rotate (xRotOffset, 0, 0);
+
+		LoadCharacter ();
 	}
 	
 	public void LoadCharacter() {
 		GameObject gs = GameObject.Find ("__GameSettings");
 
 		if( gs == null){
-			Instantiate(gameSettings, Vector3.zero, Quaternion.identity);
-			gs.name = "__GameSettings";
+			GameObject gs1 = Instantiate(gameSettings, Vector3.zero, Quaternion.identity) as GameObject;
+			gs1.name = "__GameSettings";
 		}
 
 		GameSettings gsScript = GameObject.Find ("__GameSettings").GetComponent<GameSettings>();
--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -52,7 +52,36 @@ public class GameSettings : MonoBehaviour {
 	}
 
 	public void LoadCharacterData() {
+		GameObject pc = GameObject.Find ("PC");
+		PlayerCharacter pcClass = pc.GetComponent<PlayerCharacter> ();
+		pcClass.Name = PlayerPrefs.GetString ("Player Name", "Name Me");
 
+		Debug.Log (pcClass.Name);
+
+		
+		for (int cnt = 0; cnt < Enum.GetValues(typeof(AttributeName)).Length; cnt++) {
+			PlayerPrefs.SetInt(((AttributeName)cnt).ToString() + " - Base Value", pcClass.GetPrimaryAttribute(cnt).BaseValue);
+			PlayerPrefs.SetInt(((AttributeName)cnt).ToString() + " - Exp To Level", pcClass.GetPrimaryAttribute(cnt).ExpToLevel);
+			
+		}
+		
+		for (int cnt = 0; cnt < Enum.GetValues(typeof(VitalName)).Length; cnt++) {
+			PlayerPrefs.SetInt(((VitalName)cnt).ToString() + " - Base Value", pcClass.GetVital(cnt).BaseValue);
+			PlayerPrefs.SetInt(((VitalName)cnt).ToString() + " - Exp To Level", pcClass.GetVital(cnt).ExpToLevel);
+			PlayerPrefs.SetInt(((VitalName)cnt).ToString() + " - Current Value", pcClass.GetVital(cnt).CurValue);
+			
+			PlayerPrefs.SetString(((VitalName)cnt).ToString() + " - Mods", pcClass.GetVital(cnt).GetModifyingAttributesString());
+			//pcClass.GetVital(cnt).GetModifyingAttributesString();
+		}
+		
+		for (int cnt = 0; cnt < Enum.GetValues(typeof(SkillName)).Length; cnt++) {
+			PlayerPrefs.SetInt(((SkillName)cnt).ToString() + " - Base Value", pcClass.GetSkill(cnt).BaseValue);
+			PlayerPrefs.SetInt(((SkillName)cnt).ToString() + " - Exp To Level", pcClass.GetSkill(cnt).ExpToLevel);
+			
+			PlayerPrefs.SetString(((SkillName)cnt).ToString() + " - Mods", pcClass.GetSkill(cnt).GetModifyingAttributesString());
+			
+			//pcClass.GetSkill(cnt).GetModifyingAttributesString();
+		}
 	}
 
 }
