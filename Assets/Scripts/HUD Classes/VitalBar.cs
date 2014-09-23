b3feb48fd7d6c6201e0c9311413f31bd920a1443
Corey <cthur315@gmail.com>
2014-09-22 17:02:45 -0700

    Started health bar redesign

---
+++ Assets/Scripts/HUD Classes/VitalBar.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// VitalBar.cs
+/// Sept. 22, 2014
+/// Corey Thur
+/// 
+/// This class is responsible for displaying a vital for the player character or a mob.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class VitalBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// This method is called when the gameobject is enabled
+	public void OnEnable() {
+
+	}
+
+	// This method is called when the gameobject is disabled
+	public void OnDisable() {
+
+	}
+}
