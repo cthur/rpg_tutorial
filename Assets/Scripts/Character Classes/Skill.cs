6ac0d0d914a46d6cca09b42f56f7e1f433394725
Corey <cthur315@gmail.com>
2014-10-15 11:34:51 -0700

    Commented Skills file and began working on mobs

--- Assets/Scripts/Character Classes/Skill.cs
+++ Assets/Scripts/Character Classes/Skill.cs
@@ -1,14 +1,29 @@
-﻿
+﻿/// <summary>
+/// Skill.cs
+/// Oct. 15, 2014 
+/// Corey Thur
+/// 
+/// This class contains all the extra functions that are needed for a skill.
+/// </summary>
 public class Skill : ModifiedStat {
 
-	private bool _known;
+	private bool _known;             // A boolean variable to toggle if a character knows a skill
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Skill"/> class.
+	/// </summary>
 	public Skill() {
+		UnityEngine.Debug.Log ("Skill created");
+
 		_known = false;
 		ExpToLevel = 25;
 		LevelModifier = 1.1f;
 	}
 
+	/// <summary>
+	/// Gets or sets a value indicating whether this <see cref="Skill"/> is known.
+	/// </summary>
+	/// <value><c>true</c> if known; otherwise, <c>false</c>.</value>
 	public bool Known {
 		get{ return _known; }
 		set{ _known = value; }
@@ -16,6 +31,9 @@ public class Skill : ModifiedStat {
 
 }
 
+/// <summary>
+/// A list of skills the player can learn.
+/// </summary>
 public enum SkillName {
 	Melee_Offense,
 	Melee_Defense,
