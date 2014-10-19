26aba448efc5857638e83f8ae8c0bcc6441d3125
Corey <cthur315@gmail.com>
2014-10-19 16:19:12 -0700

    Spawn points and mob generator
    Created spawn points for the various mobs
    Worked on the mob generator script

--- Assets/Scripts/MobGenerator.cs
+++ Assets/Scripts/MobGenerator.cs
@@ -3,12 +3,57 @@ using System.Collections;
 
 public class MobGenerator : MonoBehaviour {
 
+	public enum State {
+		Idle,
+		Initialize,
+		Setup,
+		SpawnMob
+	}
+
 	public GameObject[] mobPrefabs;              // An array to hold all of the prefabs of mobs that are spawned
 	public GameObject[] spawnPoints;             // This array will hold a reference to all of the spawnpoints in the scene
 
-	// Use this for initialization
-	void Start () {
-	
+	public State state;                          // This is a local variable that holds the current state
+
+	void Awake () {
+		state = State.Initialize;
 	}
 
+	// Use this for initialization
+	IEnumerator Start () {
+		while(true) {
+			switch(state) {
+			case State.Initialize:
+				Initialize();
+				break;
+			case State.Setup:
+				Setup();
+				break;
+			case State.SpawnMob:
+				SpawnMob();
+				break;
+
+			}
+
+			yield return 0;
+		}
+	}
+
+	private void Initialize() {
+		Debug.Log ("***We are in the Initialize function***");
+
+		state = State.Setup;
+	}
+
+	private void Setup() {
+		Debug.Log ("***We are in the Setup function***");
+
+		state = State.SpawnMob;
+	}
+
+	private void SpawnMob() {
+		Debug.Log ("***Spawn Mob***");
+
+		state = State.Idle;
+	}
 }
