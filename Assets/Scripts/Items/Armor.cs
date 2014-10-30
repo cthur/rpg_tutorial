90684ca2053dff674bc7baabb3c321cdae17cd86
Corey <cthur315@gmail.com>
2014-10-30 09:56:44 -0700

    Added armor, clothing and jewelry classes

---
+++ Assets/Scripts/Items/Armor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Armor : Clothing {
+
+	private int _armorLevel;           // The armor level of the current piece of armor
+
+	public Armor() {
+		_armorLevel = 0;
+	}
+
+	public Armor(int al) {
+		_armorLevel = al;
+	}
+
+	public int ArmorLevel {
+		get { return _armorLevel; }
+		set { _armorLevel = value; }
+	}
+
+}
---
+++ Assets/Scripts/Items/Jewelry.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Jewelry : BuffItem {
+
+	private JewelrySlot _slot;         // Store the slot the jewelry is in
+
+	public Jewelry() {
+		_slot = JewelrySlot.PocketItem;
+	}
+
+	public Jewelry(JewelrySlot slot) {
+		_slot = slot;
+	}
+
+	public JewelrySlot Slot {
+		get { return _slot; }
+		set { _slot = value; }
+	}
+
+}
+
+public enum JewelrySlot {
+
+	EarRings,
+	Necklaces,
+	Bracelets,
+	Rings,
+	PocketItem
+
+}
